fe3591c4d5c7dbbd2c1d6fcf8edfb9e8d2573c65
Zee Jay <zj.bulawan@gmail.com>
2025-02-10 11:42:44 +0800

    Update: Adding prop to component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
-import Contacts from "./components/Contacts"
+import Contacts from "./components/Contact"
+
+
 
 
 export default function App() {
   return (
-    <Contacts />
+    <div className="contacts">
+      <Contacts
+        img="./src/images/mr-whiskerson.png"
+        name="Mr. Whiskerson"
+        phone="(212) 555-1234"
+        email="mr.whiskaz@catnap.meow"
+      />
+      <Contacts
+        img="./src/images/fluffykins.png"
+        name="Fluffykins"
+        phone="(212) 555-2345"
+        email="fluff@me.com"
+      />
+      <Contacts
+        img="./src/images/felix.png"
+        name="Felix"
+        phone="(212) 555-4567"
+        email="thecat@hotmail.com"
+      />
+      <Contacts
+        img="./src/images/pumpkin.png"
+        name="Pumpkin"
+        phone="(0800) CAT KING"
+        email="pumpkin@scrimba.com"
+      />
+
+    </div>
   )
 }
